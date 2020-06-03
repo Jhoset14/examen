14fb679bc82a442132b809bbb036ae50e06ab28b
PC <PC@DESKTOP-ULFI8UB>
2020-06-03 11:38:46 -0500

    creacion de constructor y getter and setters y primer metodo

--- src/main/java/unibe/edu/ec/examen/Triangle.java
+++ src/main/java/unibe/edu/ec/examen/Triangle.java
@@ -1,5 +1,25 @@
 package unibe.edu.ec.examen;
 
 public class Triangle {
+private double side;
+
+public Triangle(double side) {
+	super();
+	this.side = side;
+}
+
+public double getSide() {
+	return side;
+}
+
+public void setSide(double side) {
+	this.side = side;
+}
+
+public double calculatePerimetro() {
+	double perimetro = this.side + this.side +this.side;
+	return perimetro;
+}
+
 
 }
