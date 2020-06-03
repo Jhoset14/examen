fea160fb2d06a682ccbf07de717cda43b0d38f3b
PC <PC@DESKTOP-ULFI8UB>
2020-06-03 11:43:12 -0500

    Creacion del test

---
+++ src/test/java/unibe/edu/ec/examen/TestTriangle.java
@@ -0,0 +1,19 @@
+package unibe.edu.ec.examen;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestTriangle {
+	
+	@Before
+	
+	
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
